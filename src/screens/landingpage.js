ae330db2c5a7bec12841b062bbdb65d4e9876808
Rukyy <rukemeniemogha38@gmail.com>
2024-08-05 04:14:47 +0100

    landing page mobile view

--- src/screens/landingpage.js
+++ src/screens/landingpage.js
@@ -1,5 +1,6 @@
 import "../css/landingpage.css"
 import medaipic from "../assets/med-aihome.png"
+import stars from "../assets/landing page stars.png"
 import { Link } from "react-router-dom";
 
 const Home = () => {
@@ -7,7 +8,7 @@ const Home = () => {
         <div className="homepage">
             <div className="writeupcontent">
                 <span><h1 style={{ margin: "0" }}>
-                    Evolving <h1 style={{ color: "#18A6F5", margin: "0" }}>Medicine</h1> with Ai
+                    Evolving <br /><p style={{ color: "#18A6F5", margin: "0", display:"inline" }}>Medicine</p> with <br /> AI
                 </h1></span>
                 <p>Taking the next step in the health care sector</p>
                 <div className="actionbtns">
@@ -17,7 +18,11 @@ const Home = () => {
             </div>
             <div className="imagecontent">
                 <div className="blueunderline"><img src={medaipic} alt="" srcset="" /></div>
-
+                <img className="stars" src={stars} alt="" />
+                <div className="actionbtns">
+                    <Link to={"/forms"}><button style={{ backgroundColor: "white" }}>Ai Consultation</button></Link>
+                    <Link to={"/login"}><button style={{ backgroundColor: "#18A6F5", color: "white" }}>Login</button></Link>
+                </div>
             </div>
         </div>);
 }
